e13180056e2f921c8e1f8214ce1b4054c96ac14a
Tshepiso <tlhongtshepiso2@gmail.com>
2026-04-13 14:45:58 +0200

    COMPLETED: exercise 3

--- encapuslation-excercise-3/src/main/java/com/practice/Person.java
+++ encapuslation-excercise-3/src/main/java/com/practice/Person.java
@@ -3,9 +3,34 @@ package com.practice;
 public class Person {
 
     // TODO: add private fields
+    private String name;
+    private int age;
+    private double height;
+
+    Person() {
+       this.name = name;
+       this.age = age;
+       this.height = height;
+
+    }
 
     // TODO: implement setters
 
+    public void setName(String s) {
+        this.name = s;
+    }
+
+    public void setAge(int i) {if (i > 0 && i <130)this.age = i;}
+
+    public void setHeight (double h) { if (h >0 && h < 300)this.height = h;}
     // TODO: implement getters
 
+    public String getName() {return this.name;}
+
+    public int getAge() {
+        return this.age;
+    }
+    public double getHeight(){
+      return this.height;
+    }
 }
