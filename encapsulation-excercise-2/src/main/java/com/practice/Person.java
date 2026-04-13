94e01f875416c123db66e6d0d25fc6b7f06ef9af
Tshepiso <tlhongtshepiso2@gmail.com>
2026-04-13 14:17:23 +0200

    COMPLETED: exercise 2

--- encapsulation-excercise-2/src/main/java/com/practice/Person.java
+++ encapsulation-excercise-2/src/main/java/com/practice/Person.java
@@ -2,10 +2,25 @@ package com.practice;
 
 public class Person {
 
-    // TODO: add private age field
+    // TODO: add private age field\\
+    private int age;
+    Person() {
+        this.age = age;
+    }
 
     // TODO: implement setter
 
+    public void setAge(int years){
+        if (years >0 && years < 105){
+            this.age = years;
+        }
+//        this.age = years;
+    }
+
     // TODO: implement getter
+
+    public int getAge(){
+        return this.age;
+    }
 
 }
